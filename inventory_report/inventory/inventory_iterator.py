b193b0a54921482b5b47d02a1baa99913a162137
Kristiano <kristianokasper@gmail.com>
2022-06-14 17:27:59 -0300

    Req 10 iterator

---
+++ inventory_report/inventory/inventory_iterator.py
@@ -0,0 +1,16 @@
+from abc import Iterator
+
+
+class InventoryIterator(Iterator):
+    def __init__(self, items):
+        self.items = items
+        self.pos = 0
+
+    def __next__(self):
+        try:
+            item = self.items[self.pos]
+        except IndexError:
+            raise StopIteration()
+        else:
+            self.pos += 1
+            return item
