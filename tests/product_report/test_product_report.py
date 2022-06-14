5dcb58cc32928579574b6f7ac9c6a2196e8d2081
Kristiano <kristianokasper@gmail.com>
2022-06-13 23:36:47 -0300

    Req 02

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "Nicotine Polacrilex",
+        "Target Corporation",
+        "2020-02-18",
+        "2022-09-17",
+        "CR25 1551 4467 2549 4402 1",
+        "instrucao 1",
+    )
+    result = (
+        f"O produto {product.nome_do_produto} "
+        f"fabricado em {product.data_de_fabricacao} "
+        f"por {product.nome_da_empresa} com validade "
+        f"até {product.data_de_validade} "
+        f"precisa ser armazenado {product.instrucoes_de_armazenamento}."
+    )
+    assert str(product) == result
