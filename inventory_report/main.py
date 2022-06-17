6610f5d1126d740ed35fa4e954492cdcc96f2221
Kristiano <kristianokasper@gmail.com>
2022-06-16 23:06:35 -0300

    Req 11

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,20 @@
+import sys
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
+
+
 def main():
-    pass
+    if len(sys.argv) < 3:
+        print("Verifique os argumentos", file=sys.stderr)
+    else:
+        _, path, type = sys.argv
+        file = ""
+        if path.endswith(".csv"):
+            file = InventoryRefactor(CsvImporter)
+        if path.endswith(".json"):
+            file = InventoryRefactor(JsonImporter)
+        if path.endswith(".xml"):
+            file = InventoryRefactor(XmlImporter)
+        print(file.import_data(path, type), end="")
